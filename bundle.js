ed24fe9ee3856b2daeafdf506424a377b3b0fcb1
Monty Anderson <monty.linux@gmail.com>
2017-07-17 23:48:28 +0100

    much nicer stylee

--- bundle.js
+++ bundle.js
@@ -7963,29 +7963,32 @@ utils.intFromLE = intFromLE;
 
 },{"bn.js":4,"minimalistic-assert":47,"minimalistic-crypto-utils":48}],26:[function(require,module,exports){
 module.exports={
-  "_from": "elliptic@^6.2.3",
+  "_args": [
+    [
+      "elliptic@6.4.0",
+      "/var/www/html/ethane"
+    ]
+  ],
+  "_from": "elliptic@6.4.0",
   "_id": "elliptic@6.4.0",
   "_inBundle": false,
   "_integrity": "sha1-ysmvh2LIWDYYcAPI3+GT5eLq5d8=",
   "_location": "/elliptic",
   "_phantomChildren": {},
   "_requested": {
-    "type": "range",
+    "type": "version",
     "registry": true,
-    "raw": "elliptic@^6.2.3",
+    "raw": "elliptic@6.4.0",
     "name": "elliptic",
     "escapedName": "elliptic",
-    "rawSpec": "^6.2.3",
+    "rawSpec": "6.4.0",
     "saveSpec": null,
-    "fetchSpec": "^6.2.3"
+    "fetchSpec": "6.4.0"
   },
-  "_requiredBy": [
-    "/secp256k1"
-  ],
+  "_requiredBy": [],
   "_resolved": "https://registry.npmjs.org/elliptic/-/elliptic-6.4.0.tgz",
-  "_shasum": "cac9af8762c85836187003c8dfe193e5e2eae5df",
-  "_spec": "elliptic@^6.2.3",
-  "_where": "/var/www/html/ethane/node_modules/secp256k1",
+  "_spec": "6.4.0",
+  "_where": "/var/www/html/ethane",
   "author": {
     "name": "Fedor Indutny",
     "email": "fedor@indutny.com"
@@ -7993,7 +7996,6 @@ module.exports={
   "bugs": {
     "url": "https://github.com/indutny/elliptic/issues"
   },
-  "bundleDependencies": false,
   "dependencies": {
     "bn.js": "^4.4.0",
     "brorand": "^1.0.1",
@@ -8003,7 +8005,6 @@ module.exports={
     "minimalistic-assert": "^1.0.0",
     "minimalistic-crypto-utils": "^1.0.0"
   },
-  "deprecated": false,
   "description": "EC cryptography",
   "devDependencies": {
     "brfs": "^1.4.3",
